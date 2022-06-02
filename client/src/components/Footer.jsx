2303a01cf9ee0e7f972816e870c14ecdd6fb19be
jehala <jenniferehala@github.com>
2022-06-02 12:28:25 -0400

    commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = (props) => {
     return (
         <div className={props.footer ? 'footer2' : 'footer1'}>
-            <p>Header</p>
+            <p>Footer</p>
         </div>
     )
 }
